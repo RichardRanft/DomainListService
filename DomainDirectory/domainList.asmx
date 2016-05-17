1a7aad6437fc89d699d84adf2ebceac28cb6ff73
unknown <rranft@WS-2UA2241C65.ad.agi>
2016-05-17 11:44:13 -0700

    initial commit

---
+++ DomainDirectory/domainList.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ShareList.asmx.cs" Class="DomainDirectory.DomainListService" %>
---
+++ DomainDirectory/domainList.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace DomainDirectory
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://DomainDirectory/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class DomainListService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public String GetDomainDirectory()
+        {
+            String DomainDictionary = "";
+            // load Domain name/path dictionary from file
+            using (StreamReader sr = new StreamReader(Server.MapPath("domainList.txt")))
+            {
+                String line = "";
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    if (line.Length < 1)
+                        continue;
+                    DomainDictionary += line + ";";
+                }
+            }
+            return DomainDictionary;
+        }
+    }
+}
